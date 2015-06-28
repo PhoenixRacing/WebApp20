0dd748f3915cc5bb3256746e77c535b94cba1fa9
Radmer <radmer@darksky.net>
2015-06-28 11:43:30 -0400

    adding the data collection route

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ node app.js or nodemon app.js
 var express = require('express'), //Makes the magic happen
 		logger = require('morgan'), //So we can actually see whats happening on the command line
 		path = require('path'), //makes it easier to write the path to things
-		mongoose = require('mongoose'), //Javascript Mongo db library
+		config = require("config"), 
 		bodyParser = require('body-parser'), 
 		cookieParser = require('cookie-parser'),
 		session = require('express-session'), //Sessions for user login
@@ -15,14 +15,14 @@ var express = require('express'), //Makes the magic happen
 
 //Schemas will go here
 
-//routefiles will go here
-
+//route files will go here
+var dataCollection = require('./routes/data.js')
 
 //initialize the app and connect DB
 var app = express();
-var mongoURI = process.env.MONGOURI || "mongodb://localhost/test";
+
 var PORT = process.env.PORT || 3000;
-mongoose.connect(mongoURI);
+
 
 //attach userful middleware
 app.use(logger('dev'));
@@ -42,6 +42,8 @@ app.get('/', function(req, res){
   var url = path.resolve( __dirname + '/views/index.html');
   res.sendFile(url);
 })
+
+app.post('/mobiledata', dataCollection.postData)
 //start the app
 app.listen(PORT, function() {
   console.log("Application running on port:", PORT);
