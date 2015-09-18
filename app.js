10e8680506fd6b8e5586741a99eaf279eb8b7ac6
deborahhellen <hellen.deborahv@gmail.com>
2015-09-17 20:22:31 -0400

    Setting up app to work without db for now

--- app.js
+++ app.js
@@ -1,50 +1,31 @@
-/*
-Entry point into the express app. Run using either
-node app.js or nodemon app.js
-*/
+//Require npm modules 
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-//Import the necessary libraries for the app
-var express = require('express'), //Makes the magic happen
-		logger = require('morgan'), //So we can actually see whats happening on the command line
-		path = require('path'), //makes it easier to write the path to things
-		config = require("config"), 
-		bodyParser = require('body-parser'), 
-		cookieParser = require('cookie-parser'),
-		session = require('express-session'), //Sessions for user login
-		passport = require('passport') //passport to handle actual login
-
-//Schemas will go here
 
 //route files will go here
-var dataCollection = require('./routes/data.js')
+// var dataCollection = require('./routes/data.js')
 
-//initialize the app and connect DB
+// Initialize express app
 var app = express();
 
-var PORT = process.env.PORT || 3000;
-
-
-//attach userful middleware
-app.use(logger('dev'));
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: false }));
+//Middleware
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-// app.use(session({
-//     secret: 'This is not a secret ;)',
-//     resave: false,
-//     saveUninitialized: true
-// }));
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function(req, res){
-  var url = path.resolve( __dirname + '/views/index.html');
-  res.sendFile(url);
-})
 
-app.post('/mobiledata', dataCollection.postData)
-//start the app
-app.listen(PORT, function() {
-  console.log("Application running on port:", PORT);
+//API routes that angular will use to get and post data 
+app.get("/api/home", function(req, res) {
+	res.send("Hello world");
 });
+
+var PORT = 3000;
+
+app.listen(process.env.PORT || PORT);
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,12 @@
 (function(){
 	//initialize the angular app and inject dependencies.
-	var app = angular.module("olinbaja", ['ngRoute'])
+	var app = angular.module("olinbaja", ['ngRoute']);
+
+	app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl : './pages/home.html',
+        });
+    });
+
 })();
