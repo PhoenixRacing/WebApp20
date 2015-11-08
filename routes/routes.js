eb185df924c4c16f2c61dfc36a7a47092663381b
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2015-11-08 15:05:11 -0500

    Uploading files works!!

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,6 @@
 var path = require('path');
+var fs = require('fs');
+var multiparty = require('multiparty');
 
 module.exports = function(app, passport) {
 
@@ -7,7 +9,7 @@ module.exports = function(app, passport) {
 	});
 
 	app.get("/login", function(req, res) {
-		console.log("Get login")
+		console.log("Get login");
 		res.sendFile(path.join(__dirname, '../views', 'login.html'));
 	});
 
@@ -29,6 +31,56 @@ module.exports = function(app, passport) {
 	app.get("/profile", isLoggedIn, function(req, res) {
 		res.sendFile(path.join(__dirname, '../views', 'profile.html'));
 	});
+
+	app.get("/uploadImage", function(req, res) {//isLoggedIn, function(req, res) {
+		res.sendFile(path.join(__dirname, '../views', 'uploadImage.html'));
+	});
+
+	app.post("/uploadImage", function(req, res) {//isLoggedIn, function(req, res) {
+		var form = new multiparty.Form();
+
+		form.parse(req, function(err, fields, files) {
+
+			var img = files.image[0];
+			console.log(img);
+			var filename = img.originalFilename.split(".")
+			var filetype = filename[filename.length-1].toLowerCase();
+
+			// figure out if it's actually an image
+			var allowedTypes = ["jpg","jpeg","png","gif"];
+			if (allowedTypes.indexOf(filetype) == -1) {
+				console.log("ERROR. File must be an image.");
+
+				// delete the temp file
+				fs.unlink(img.path, null);
+				res.redirect("/uploadImage");
+				return;
+			}
+
+			fs.readFile(img.path, function (err, data) {
+
+				// delete the temp file
+				fs.unlink(img.path, null);
+
+				if (err) {
+					console.log(err);
+					return;
+				}
+
+				// rename the file to milliseconds
+				var time = new Date().getTime();
+				var newPath = path.join(__dirname, '../uploads', String(time) + "." + filetype);
+
+				fs.writeFile(newPath, data, function (err) {
+					if (err) {
+						console.log(err);
+					}
+					res.redirect("/uploadImage");
+				});
+			});
+		});
+
+	})
 
 	// route middleware to make sure a user is logged in
 	function isLoggedIn(req, res, next) {
