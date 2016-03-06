60edfb48df80ef359d44d536052346b07d8b087f
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2016-03-06 17:08:42 -0500

    Uploads to s3 now

--- app.js
+++ app.js
@@ -48,11 +48,15 @@ var routes = require('./routes/routes.js');
 var auth = require('./routes/auth.js');
 var donor = require('./routes/donor.js');
 var team = require('./routes/team.js');
+var purchasing = require('./routes/purchasing.js');
+var upload = require('./routes/upload.js');
 
 app.use("/", routes);
 app.use("/auth", auth);
 app.use("/donor", donor);
 app.use("/team", team);
+app.use("/purchasing", purchasing);
+app.use("/upload", upload);
 
 app.get("/test", function(req, res) {
   var url = path.resolve( __dirname + '/test.html');
