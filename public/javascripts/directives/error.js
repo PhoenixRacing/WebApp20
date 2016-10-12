1c62437e22d9d72825350651068d39edb728bdba
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2016-10-11 21:01:01 -0400

    Specify function name

--- public/javascripts/directives/error.js
+++ public/javascripts/directives/error.js
@@ -8,7 +8,7 @@
       replace: true,
       templateUrl: '../../pages/error.html',
       controller: function($scope) {
-        $scope.close = function($event) {
+        $scope.closeError = function($event) {
           var errorText = $event.target.parentElement.innerText;
           var end = errorText.substr(errorText.length - 5);
           // This should change and won't be needed if we use an 'x' icon.
